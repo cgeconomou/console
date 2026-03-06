fb5a95a887255ff0e38722bcf9b93a9a85c6b67f
Costas Economou <cgeconomou@hotmail.com>
2026-03-06 11:16:46 +0200

    new test v4

--- main.js
+++ main.js
@@ -13,6 +13,21 @@ function runCommand(cmd) {
     console.log(stdout);
   });
 }
+function getUser(db, userId) {
+  // BAD: user input directly concatenated into query
+  const query = "SELECT * FROM users WHERE id = '" + userId + "'";
+  return db.query(query);
+}
+
+const { exec } = require("child_process");
+
+function runCommand(cmd) {
+  // BAD: unsanitized user input executed as a shell command
+  exec(cmd, (err, stdout) => {
+    if (err) console.error("Error:", err);
+    console.log(stdout);
+  });
+}
 const { exec } = require("child_process");
 
 function runCommand(cmd) {
